da03b7ea0a960d9c10ef07a4c7b11c848cc5bdf2
Exinfinite chen <chen.min.home@gmail.com>
2020-07-23 16:10:44 +0800

    fixed dom html5 block support

--- src/Render/Dom.php
+++ src/Render/Dom.php
@@ -8,21 +8,33 @@ class Dom extends \DOMDocument {
         parent::__construct($version, $encoding);
         $this->encoding = $encoding;
         $this->tidy = new \tidy;
+        $this->parseRules = [
+            'clean' => TRUE,
+            'doctype' => 'omit',
+            'show-body-only' => true,
+            'indent' => 2,
+            'output-html' => TRUE,
+            'tidy-mark' => FALSE,
+            'wrap' => 0,
+            'new-blocklevel-tags' => 'article aside audio bdi canvas details dialog figcaption figure footer header hgroup main menu menuitem nav section source summary template track video',
+            'new-empty-tags' => 'command embed keygen source track wbr',
+            'new-inline-tags' => 'audio command datalist embed keygen mark menuitem meter output progress source time video wbr',
+        ];
     }
     function loadHTML($content, $options = 0) {
         libxml_use_internal_errors(true);
         parent::loadHTML(
-            @mb_convert_encoding($this->tidy->repairString($content, [], 'utf8'), 'HTML-ENTITIES', $this->encoding),
+            @mb_convert_encoding(
+                tidy_parse_string($content, $this->parseRules, 'utf8'),
+                'HTML-ENTITIES',
+                $this->encoding
+            ),
             LIBXML_COMPACT
         );
         return $this;
     }
     function saveHtml() {
-        return $this->tidy->repairString(
-            @mb_convert_encoding(parent::saveHtml(), $this->encoding, 'HTML-ENTITIES'),
-            ['show-body-only' => true],
-            'utf8'
-        );
+        return parent::saveHtml();
     }
     function domByTagName($tag) {
         return $this->getElementsByTagName($tag);
